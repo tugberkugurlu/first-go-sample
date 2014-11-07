2e11f0e9b5f8ce221488d54fe48ec89272eed852
tugberkugurlu <tugberkugurlu@gmail.com>
2014-11-07 16:06:12 +0200

    added variables sample - part 2

--- hello/hello.go
+++ hello/hello.go
@@ -8,4 +8,16 @@ func main() {
 	var message string = "Hello Go World!"
 	var a, b, c int = 1, 2, 3
 	fmt.Println(message, a, b, c)
+	
+	// you don't need to specify types (in some cases I guess?). They can be infered.
+	
+	var message2 = "Hello Go World!"
+	var d, e, f = 1, 2, 3
+	fmt.Println(message2, d, e, f)
+	
+	// := operator does the declaration and initialization at the same time
+	
+	message3 := "Hello Go World!"
+	g, h, i := 1, 2, 3
+	fmt.Println(message3, g, h, i)
 }
